7be7563096004529bb95217caf27ab5bc881736d
Daniel Ng <ng17daniel@gmail.com>
2026-04-18 19:18:00 -0400

    SurfaceMaxxing

--- Assets/Scripts/DirtySurfaceBehavior.cs
+++ Assets/Scripts/DirtySurfaceBehavior.cs
@@ -2,17 +2,26 @@ using UnityEngine;
 
 public class DirtySurfaceBehavior : MonoBehaviour
 {
+    Renderer rend;
     Material mat;
-    [SerializeField] float maxDirtiness = 1f;
+    [SerializeField] float maxDirtiness = 10f;
+
+    float tempTimer = 0f;
 
     private void Awake()
     {
-        mat = GetComponent<Renderer>().material;
-        mat.SetVector("Scale", transform.localScale);
+        rend = GetComponent<Renderer>();
+        mat = rend.material;
+        mat.SetVector("_Scale", rend.bounds.size);
     }
 
     void Update()
     {
-        mat.SetVector("_Scale", transform.localScale);
+        // mat.SetVector("_Scale", transform.localScale);
+        if (tempTimer < maxDirtiness)
+        {
+            tempTimer += Time.deltaTime;
+            mat.SetFloat("_Dirtiness", tempTimer / maxDirtiness);
+        }
     }
 }
