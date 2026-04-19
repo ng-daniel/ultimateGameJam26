1af7c75e4a331efb6d445d07ea6fa064080ac5fb
Daniel Ng <ng17daniel@gmail.com>
2026-04-19 11:05:24 -0400

    main menu from title to gameplay

---
+++ Assets/Scripts/MainMenu/MenuManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] RoomQueue roomQueue;
+
+    [SerializeField] GameObject mainMenuUI;
+    [SerializeField] GameObject levelSelectUI;
+    public void PlayLevel(RoomConfig config)
+    {
+        if (roomQueue != null)
+        {
+            roomQueue.config = config;
+            Debug.Log("Assigned RoomConfig to RoomQueue: " + config.name);
+            SceneManager.LoadScene("MainGame");
+        }
+        else
+        {
+            Debug.LogError("RoomQueue not found in the scene. Please ensure a RoomQueue ScriptableObject is present.");
+        }
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenuUI.SetActive(true);
+        levelSelectUI.SetActive(false);
+    }
+    public void ShowLevelSelect()
+    {
+        mainMenuUI.SetActive(false);
+        levelSelectUI.SetActive(true);
+    }
+
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
